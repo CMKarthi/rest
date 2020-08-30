aa028d34754cd016e6447182d1910f03d24c874a
Karthikeyani <cmkarthidoc@gmail.com>
2020-08-30 10:33:48 -0500

    Hashmap Added

--- src/main/java/com/karthi/rest/controllers/StudentController.java
+++ src/main/java/com/karthi/rest/controllers/StudentController.java
@@ -4,34 +4,29 @@ import com.karthi.rest.model.Student;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class StudentController {
+    HashMap<Long, Student> hashStudent = new HashMap<Long, Student>();
+    Long hashStudentKey =1L;
 
     @GetMapping("/students/{id}")
     Student getStudent(@PathVariable Long id) {
-        return new Student(id, "Samyu");
+        return (hashStudent.get(id));
     }
     @GetMapping("/students")
-    List<Student> getStudents() {
-        Student s1 = new Student(1L, "Samyu");
-        Student s2 = new Student(2L,"Cherry");
-        Student s3 = new Student(3L,"Karthi");
-        Student s4 = new Student(4L,"Saran");
-        List<Student> studentList = new ArrayList<>();
-        studentList.add(s1);
-        studentList.add(s2);
-        studentList.add(s3);
-        studentList.add(s4);
-        return studentList;
+    HashMap<Long, Student> getStudents() {
+
+        return hashStudent;
     }
 
     @PostMapping("/students")
-    public void postStudent(@RequestBody Student newStudent)
-    {
-        System.out.println("Id:  "+newStudent.getId());
-        System.out.println("Name :  "+newStudent.getName());
+    public void postStudent(@RequestBody Student newStudent) {
 
+        hashStudent.put(hashStudentKey++,newStudent);
+       // hashStudent.put(newStudent.getId(),newStudent);
     }
 }
