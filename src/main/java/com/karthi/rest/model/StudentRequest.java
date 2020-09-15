1ec245540222c7dcf516b6fc6329289ef3ab6c59
Karthikeyani <cmkarthidoc@gmail.com>
2020-09-15 15:35:50 -0500

    Added Response Features 400 and 404

--- src/main/java/com/karthi/rest/model/StudentRequest.java
+++ src/main/java/com/karthi/rest/model/StudentRequest.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @Getter
 @Setter
 public class StudentRequest {
+    @NotNull
+    @Size(min=2, message="The Name should have a minimum of 2 Characters")
     String name;
+    @NotNull
+    @Size(min=1, message="The Gender should have a minimum of 1 Character")
     String gender;
 }
